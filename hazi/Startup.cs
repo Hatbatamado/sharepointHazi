9f1465e034bc92b08aee8712d3acf5d680db2190
Nakia <nakiaaresz@gmail.com>
2015-03-03 15:58:33 +0100

    ures

---
+++ hazi/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hazi.Startup))]
+namespace hazi
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
