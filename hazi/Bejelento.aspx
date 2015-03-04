52d14be8e64e8b549ab78d5b199405d4515e5f22
Nakia <nakiaaresz@gmail.com>
2015-03-04 21:20:39 +0100

    kivetelkezes elkezdese

--- hazi/Bejelento.aspx
+++ hazi/Bejelento.aspx
@@ -1,6 +1,7 @@
 ﻿
 <%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Bejelento.aspx.cs" Inherits="hazi.Bejelento" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <!-- http://jqueryui.com/datepicker/ jqueryui eleje -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
     <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
@@ -10,12 +11,13 @@
             $("#<%=datepicker2.ClientID%>").datepicker();
         });
     </script>
+    <!-- jqueryui vége -->
 
     <asp:Label ID="Label1" runat="server" Text="Folyamat kezdése:"></asp:Label>
     <asp:TextBox ID="datepicker1" runat="server"></asp:TextBox>
     <asp:Label ID="Label2" runat="server" Text="Folyamat befejezése:"></asp:Label>
     <asp:TextBox ID="datepicker2" runat="server"></asp:TextBox>
     <asp:DropDownList ID="DropDownList1" runat="server" ItemType="hazi.Models.Jogcim" SelectMethod="GetJogcimek" DataTextField="Cim"></asp:DropDownList>
-    <asp:Button ID="save" runat="server" Text="Mentés" />
+    <asp:Button ID="save" runat="server" Text="Mentés" OnClick="save_Click" />
     <asp:Button ID="cancel" runat="server" Text="Mégse" OnClick="cancel_Click" />
 </asp:Content>
--- hazi/Bejelento.aspx.cs
+++ hazi/Bejelento.aspx.cs
@@ -8,13 +8,26 @@ using System.Web.UI.WebControls;
 
 namespace hazi
 {
+    enum hibak { nincsHiba,
+                 elsoDatumRegebbiMainal,
+                 masodikDatumRegebbiMainal,
+                 elsoDatumNincsKivalasztva,
+                 masodikDatumNincsKivalasztva,
+                 befejezesIdejeKezdesElottVan };
+
     public partial class Bejelento : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Page.IsPostBack)
+            {
+                //csak olvasható lehet a dátum kiválasztva, így több hibát is elkerülünk
+                datepicker1.ReadOnly = true;
+                datepicker2.ReadOnly = true;
+            }
         }
 
+        //Jogcimek kiolvasása DB-ből
         public IQueryable<Jogcim> GetJogcimek()
         {
             var _db = new hazi.Models.BejelentesContext();
@@ -24,7 +37,51 @@ namespace hazi
 
         protected void cancel_Click(object sender, EventArgs e)
         {
+            //Home-ra navigálás
             HttpContext.Current.Response.Redirect("./");
+        }
+
+        protected void save_Click(object sender, EventArgs e)
+        {
+            hibak hiba = hibak.nincsHiba;
+            //van-e kiválasztva dátum midenhol
+            if (datepicker1.Text != null && datepicker2.Text != null)
+            {
+                DateTime datum1, datum2;
+                string[] seged;
+                
+                //az első dátum régebbi-e mainál
+                seged = datepicker1.Text.Split('/');
+                datum1 = new DateTime(Int32.Parse(seged[2]), Int32.Parse(seged[1]), Int32.Parse(seged[0]));
+                if (datum1 < DateTime.Now)
+                    hiba = hibak.elsoDatumRegebbiMainal;
+
+                //a második dátum régebbi-e mainál
+                seged = datepicker2.Text.Split('/');
+                datum2 = new DateTime(Int32.Parse(seged[2]), Int32.Parse(seged[1]), Int32.Parse(seged[0]));
+                if (datum2 < DateTime.Now)
+                    hiba = hibak.masodikDatumRegebbiMainal;
+
+                //befejezés ideje kezdés után van-e
+                if (datum1 > datum2)
+                    hiba = hibak.befejezesIdejeKezdesElottVan;
+            }
+            else
+            {
+                //első dátum ki van-e választva
+                if (datepicker1.Text == null)
+                    hiba = hibak.elsoDatumNincsKivalasztva;
+                
+                //második dátum ki van-e választva
+                if (datepicker2.Text == null)
+                    hiba = hibak.masodikDatumNincsKivalasztva;
+            }
+
+            //van-e valamilyen hiba mentés előtt
+            if (hiba == hibak.nincsHiba)
+            {
+
+            }
         }
 
     }
